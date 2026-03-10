f234178b0fecbceb6c1e30da9ef03c46b3b5a3b8
ivonne-br <karinabzn@outlook.com>
2026-03-10 11:27:09 -0600

    Primer commit del proyecto RecargaCredencial

---
+++ src/main/java/com/recarga/RecargaCredencial/RecargaCredencialApplication.java
@@ -0,0 +1,13 @@
+package com.recarga.RecargaCredencial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecargaCredencialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecargaCredencialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recarga/RecargaCredencial/RecargaCredencialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recarga.RecargaCredencial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecargaCredencialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
